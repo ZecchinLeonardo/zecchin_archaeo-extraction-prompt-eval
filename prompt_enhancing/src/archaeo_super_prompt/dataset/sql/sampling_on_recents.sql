4b110042f5c6e685e1a56b4627760473f03f1aab
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-20 10:32:03 +0200

    fix: sql syntax

--- prompt_enhancing/src/archaeo_super_prompt/dataset/sql/sampling_on_recents.sql
+++ prompt_enhancing/src/archaeo_super_prompt/dataset/sql/sampling_on_recents.sql
@@ -1,8 +1,8 @@
-select
-    *
+select *
 from
     intervention_data
-    ORDER BY RANDOM()
-    LIMIT %(max_number)s
 where
-    TO_DATE("building.Data Protocollo", 'DD-MM-YYYY') >= TO_DATE('01-01-2015', 'DD-MM-YYYY')
+    TO_DATE("building.Data Protocollo", 'DD-MM-YYYY')
+    >= TO_DATE('01-01-2015', 'DD-MM-YYYY')
+order by RANDOM()
+limit %(max_number)s
