c0b0d005c49a7d3a92e19ade6cfc382c58c7f218
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-06-20 10:12:05 +0200

    fix: sql date check

--- prompt_enhancing/src/archaeo_super_prompt/dataset/sql/sampling_on_recents.sql
+++ prompt_enhancing/src/archaeo_super_prompt/dataset/sql/sampling_on_recents.sql
@@ -5,4 +5,4 @@ from
     ORDER BY RANDOM()
     LIMIT %(max_number)s
 where
-    TO_DATE("building.Data Protcollo", 'DD-MM-YYYY') >= TO_DATE('01-01-2015', 'DD-MM-YYYY')
+    TO_DATE("building.Data Protocollo", 'DD-MM-YYYY') >= TO_DATE('01-01-2015', 'DD-MM-YYYY')
