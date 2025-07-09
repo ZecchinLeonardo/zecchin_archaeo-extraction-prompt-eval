606afd2e8d06ef4fc24673d0bbf14ae8c6209017
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-07-09 15:57:47 +0200

    feat: thesaurus fuzzy matching
    TODO: test the ner + matching on a one field with its thesaurus

--- prompt_enhancing/src/archaeo_super_prompt/models/entity_extractor/model.py
+++ prompt_enhancing/src/archaeo_super_prompt/models/entity_extractor/model.py
@@ -1,5 +1,6 @@
-from typing import Optional, Set, cast, List
+from typing import Generator, Optional, Set, Tuple, cast, List
 import requests
+import fuzzywuzzy.process as fzwz
 
 from .types import CompleteEntity, NerOutput, NerXXLEntities
 
@@ -61,7 +62,52 @@ def filter_entities(
     complete_entity_sets: List[Set[CompleteEntity]],
     allowed_entities: Set[NerXXLEntities],
 ) -> List[Set[CompleteEntity]]:
+    """For each text chunk, keep only the entities included in the given group
+    of allowed entity types.
+    """
     return [
         set(filter(lambda e: e.entity in allowed_entities, s))
         for s in complete_entity_sets
     ]
+
+
+def extract_wanted_entities(
+    complete_entity_sets: List[Set[CompleteEntity]],
+    wanted_entities: Set[str],
+    distance_treshold: float,
+) -> List[List[Tuple[CompleteEntity, List[str]]]]:
+    """Filter only the entities that fuzzymatch with wanted thesaurus
+
+    Arguments:
+    * complete_entity_sets: a set for each text chunk of occurring entities
+    only in a group of entity types
+    * wanted_entities: a set of wanted string values to be extracted in the
+    same group of entity types
+    * distance_treshold: a float between 0 and 1
+
+    ReturnType:
+    A list for each text chunk of the matched entities. Each of them are paired
+    with the thesaurus values they have matched above the given distance
+    treshold.
+    """
+
+    def aux(complete_entity_set: Set[CompleteEntity]):
+        return [
+            (
+                entity,
+                [
+                    matched_thesaurus
+                    for matched_thesaurus, _ in cast(
+                        Generator[Tuple[str, int]],
+                        fzwz.extractWithoutOrder(
+                            entity.word,
+                            wanted_entities,
+                            score_cutoff=int(distance_treshold * 100),
+                        ),
+                    )
+                ],
+            )
+            for entity in complete_entity_set
+        ]
+
+    return [aux(ces) for ces in complete_entity_sets]
