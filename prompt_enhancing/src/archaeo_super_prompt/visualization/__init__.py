c944ed3c82bd335effe93d8559409374a68fe9f5
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-07-22 12:45:31 +0200

    fix: reset dash server
    now, it is possible, to rerun a dash server in the notebook without
    error

--- prompt_enhancing/src/archaeo_super_prompt/visualization/__init__.py
+++ prompt_enhancing/src/archaeo_super_prompt/visualization/__init__.py
@@ -7,10 +7,11 @@ from .display_fields import display_results
 from .file_explorer import add_file_explorer
 
 _app = Dash()
-_is_display_server_running = False
 
 
 def init_complete_vizualisation_engine(results: DataFrame[ResultSchema]):
+    global _app
+    _app = Dash()
     resultChildren, c1 = display_results(results)
     fileExplorer, c2 = add_file_explorer()
     _app.layout = [
@@ -22,8 +23,4 @@ def init_complete_vizualisation_engine(results: DataFrame[ResultSchema]):
 
 
 def run_display_server():
-    global _app
-    global _is_display_server_running
-
-    if not _is_display_server_running:
-        _app.run()
+    _app.run()
