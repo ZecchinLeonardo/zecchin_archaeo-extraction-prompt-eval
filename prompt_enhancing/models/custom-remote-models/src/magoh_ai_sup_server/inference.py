cf53f1d59bc275fa183d31e78483ceca63d97f0c
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-07-29 12:53:42 +0200

    feat: better gpu handling in remote ner model

--- prompt_enhancing/models/custom-remote-models/src/magoh_ai_sup_server/inference.py
+++ prompt_enhancing/models/custom-remote-models/src/magoh_ai_sup_server/inference.py
@@ -4,18 +4,6 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 import torch
 from typing import cast, List, Callable, Dict, Any
-from os import getenv
-
-def get_devices():
-    """Get the desired gpu identifiers from a env variable."""
-    device = getenv("CUDA_VISIBLE_DEVICES")
-    if device is None:
-        raise EnvironmentError("Missing the env var MAGOH_GPU_DEVICES")
-    if "," in device:
-        return device
-    if not device.isdigit():
-        raise EnvironmentError("You must precise an integer as device.")
-    return int(device)
 
 tokenizer = AutoTokenizer.from_pretrained(
     "DeepMount00/Italian_NER_XXL", torch_dtype="auto"
@@ -26,7 +14,8 @@ model = AutoModelForTokenClassification.from_pretrained(
     torch_dtype="auto",
 )
 
-nlp = pipeline("ner", model=model, tokenizer=tokenizer, device=get_devices())
+# set CUDA_VISIBLE_DEVICES, so 0 index will be mapped to an allowed GPU
+nlp = pipeline("ner", model=model, tokenizer=tokenizer, device=0)
 example = """Il commendatore Gianluigi Alberico De Laurentis-Ponti, con residenza legale in Corso Imperatrice 67,  Torino, avente codice fiscale DLNGGL60B01L219P, è amministratore delegato della "De Laurentis Advanced Engineering Group S.p.A.",  che si trova in Piazza Affari 32, Milano (MI); con una partita IVA di 09876543210, la società è stata recentemente incaricata  di sviluppare una nuova linea di componenti aerospaziali per il progetto internazionale di esplorazione di Marte."""
 
 
