6862e249d1275ee9d68528a5c33e56934282a991
Quirino <quirino.saraceni@phd.unipi.it>
2025-10-29 11:29:53 +0100

    funzionario extractor method for thesauri

---
+++ prompt_enhancing/src/archaeo_super_prompt/dataset/thesauri/funzionario.py
@@ -0,0 +1,15 @@
+def load_funzionario_candidates():
+    return list(
+        enumerate([
+            "Direttore",
+            "Direzione",
+            "Direzione Scintifica",
+            "Funzionario",
+            "Funzionario competente",
+            "Funzionario responsabile",
+            "Responsabile",
+            "Responsabile archeologo",
+            "Responsabile scientifico",
+            "Responsabile tecnico",
+        ])
+    )
